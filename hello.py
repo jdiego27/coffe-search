209fc3aa21b1b26be2ddfcc87d0a41dc0f013961
Juan Leon <leonrivasjd@gmail.com>
2025-02-25 10:19:58 -0500

    updated

--- hello.py
+++ hello.py
@@ -15,7 +15,7 @@ def index():
 def page_not_found(e):
 	return render_template("404.html"), 404
 
-# Internal Server Error
+# Internal Server Error thing updated
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
