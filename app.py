7f7ef31f8657fc27d7fb772ec7b96335eefa4a0b
Juan Leon <leonrivasjd@gmail.com>
2025-02-26 10:02:51 -0500

    added again

--- app.py
+++ app.py
@@ -25,15 +25,5 @@ def home():
     return render_template('home.html', coffee_shops=filtered_shops)
 
 
-# Invalid URL
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template("404.html"), 404
-
-# Internal Server Error
-@app.errorhandler(500)
-def page_not_found(e):
-	return render_template("500.html"), 500
-
 if __name__ == "__main__":
     app.run(debug=True)
